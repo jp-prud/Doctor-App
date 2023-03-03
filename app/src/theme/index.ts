d6cbfa88de18ef8152bfb0b25e9fc207d57d3d1e
Joao Pedro Prudencio <jpprud.contato@gmail.com>
2023-03-03 08:41:01 -0300

    feat: create styleguide and theme provider

---
+++ app/src/theme/index.ts
@@ -0,0 +1,34 @@
+export default {
+  COLORS: {
+    WHITE: "#FAFAFA",
+
+    GREEN_700: "#00875F",
+    GREEN_500: "#00B37E",
+
+    RED: "#F75A68",
+    RED_DARK: "#AA2834",
+
+    GRAY_700: "#121214",
+    GRAY_600: "#202024",
+    GRAY_500: "#29292E",
+    GRAY_400: "#333333",
+    GRAY_300: "#8391A1",
+    GRAY_200: "#C4C4CC",
+    GRAY_100: "#FAFAFA",
+  },
+  FONT_FAMILY: {
+    LIGHT: "Urbanist_300Light",
+    REGULAR: "Urbanist_400Regular",
+    MEDIUM: "Urbanist_500Medium",
+    SEMIBOLD: "Urbanist_600SemiBold",
+    BOLD: "Urbanist_700Bold",
+  },
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 28,
+    XXXL: 32
+  },
+};
